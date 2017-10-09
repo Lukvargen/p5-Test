235a083a690ac629ea7667d8a7cfbab7d37b230e
Lukvargen <lukas.magnusson@hotmail.com>
2017-10-09 22:26:37 +0200

    commit

--- p5 project 1/sketch.js
+++ p5 project 1/sketch.js
@@ -1,24 +1,40 @@
 
+var created = 0;
+var increment = 1;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	//cnv.mousePressed(pressed = true)
-	for (var x = 0; x <= windowWidth; x += 32){
-		for (var y = 0; y<= windowHeight; y += 32){
-			fill(255, 0, 30)
-			ellipse(x,y,32,32)
+	frameRate(60);
+	reset();
+
+
+}
+
+function draw() {
+
+	if (mouseIsPressed) {
+		fill(created, created, created);
+		ellipse(mouseX, mouseY, 50, 50);
+		created += increment;
+		if (created == 255 || created == 0){
+			increment *= -1;
 		}
 	}
 
 
 }
 
-function draw() {
-	
-	fill(255, 255, 255);
-	if (mouseIsPressed) {
-		ellipse(mouseX, mouseY, 50, 50);
+function keyPressed(){
+	reset()
+}
+
+function reset(){
+	background(0,0,0);
+	//cnv.mousePressed(pressed = true)
+	for (var x = 0; x <= windowWidth; x += 16){
+		for (var y = 0; y<= windowHeight; y += 16){
+			fill(100, 100, 100)
+			ellipse(x,y,32,32)
+		}
 	}
-
-
 }
